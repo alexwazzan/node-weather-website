39c94dea9a34b4bf44441b85d0ec004f1a56fd36
Jad Wazzan <alexwazzan1@gmail.com>
2020-03-26 00:08:22 -0700

    Added data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Error: ' + body.error, undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability * 100 + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability * 100 + '% chance of rain. The high and low temperatures of today are ' + body.daily.data[0].temperatureHigh + ' and ' + body.daily.data[0].temperatureLow + ' degrees, respectively.')
         }
     })
 }
