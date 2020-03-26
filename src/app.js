10bae88970afc7bfe6950e894e397975f1772db6
Jad Wazzan <alexwazzan1@gmail.com>
2020-03-26 03:19:19 -0700

    Removed unnecessary code

--- src/app.js
+++ src/app.js
@@ -66,17 +66,6 @@ app.get('/weather', (req, res) => {
     })
 })
 
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'Please provide a search term.'
-        })
-    }
-    res.send({
-        products: []
-    })
-})
-
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
