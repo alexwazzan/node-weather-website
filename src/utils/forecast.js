1eb90c08cc20e2e002d2b713b6493153e353abdd
Jad Wazzan <alexwazzan1@gmail.com>
2020-03-25 22:14:09 -0700

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/debef9f02feb7eadfcd778c874d03f44/' + latitude + ',' + longitude
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service.', undefined)
+        } else if (body.error) {
+            callback('Error: ' + body.error, undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability * 100 + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
